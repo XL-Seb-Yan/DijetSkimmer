531c6df75f0557b20748fb3d22ec97ef3663644e
David <david.renhwa.yu@gmail.com>
2019-03-15 07:17:35 +0100

    Update crab configs

--- skim/crab/skim_Res1ToRes2GluTo3Glu_M1-7000_R-0p9-madgraph-pythia8_cfg.py
+++ skim/crab/skim_Res1ToRes2GluTo3Glu_M1-7000_R-0p9-madgraph-pythia8_cfg.py
@@ -3,19 +3,27 @@ from WMCore.Configuration import Configuration
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = Configuration()
-job_name = "DijetSkim_0_0_1"
+job_name = "DijetSkim_1_0_0"
 
 config.section_("General")
 config.General.requestName = job_name
-config.General.transferLogs=True
+config.General.transferLogs = False
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 
 # Setup the custom executable
 config.JobType.psetName = os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/PSet.py') # CRAB modifies this file to contain the input files and lumis
 config.JobType.scriptExe = os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/crab_shell.sh') # CRAB then calls scriptExe jobId <scriptArgs>
-config.JobType.scriptArgs = ["source=mc"]
-config.JobType.inputFiles = [os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/crab_meat.py'), os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/NanoAODTools/scripts/haddnano.py')] #hadd nano will not be needed once nano tools are in cmssw
+config.JobType.scriptArgs = ["source=mc", "year=2017"]
+config.JobType.inputFiles = [
+	os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/crab_meat.py'), 
+	os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/NanoAODTools/scripts/haddnano.py'), #hadd nano will not be needed once nano tools are in cmssw
+	os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/skim_branches_data.txt'),
+	os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/skim_branches_mc.txt'),
+	os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/skim_branches.txt'),
+	#os.path.expandvars('$CMSSW_BASE/src/PhysicsTools/DijetSkimmer/skim/FrameworkJobReport.xml'),
+	]
+config.JobType.outputFiles = ["nanoskim.root", "hists.root"]
 config.JobType.sendPythonFolder	 = True
 config.JobType.allowUndistributedCMSSW = True
 config.section_("Data")
